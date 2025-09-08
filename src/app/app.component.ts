84bf76378be6a93f13f6a28f91a07304c454d5e3
leonardolucusi <lucusihiu@gmail.com>
2025-09-08 13:03:21 -0300

    feat: more sections added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {HeroComponent} from './hero/hero.component';
+import {AboutComponent} from './about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
